50a3a92bcae8609d7183e383534d7ffb92c1d876
Javid Afandiyev <afandiyevcavid@gmail.com>
2022-01-23 11:05:19 +0400

    test commit 10

--- src/main/java/az/interestmap/interestmap/service/impl/PlaceServiceImpl.java
+++ src/main/java/az/interestmap/interestmap/service/impl/PlaceServiceImpl.java
@@ -41,16 +41,10 @@ public class PlaceServiceImpl implements PlaceService {
                                                                  List<InterestDTO> userInterestList) {
         // FIXME vaxt qıtlığından hesablama əvəzinə mock datalar generate olundu.
 
-//        System.out.println("User lat: " + userLatitude);
-//        System.out.println("user lng: " + userLongitude);
-
         ClientSearchResponseDTO clientSearchResponseDTO1 = new ClientSearchResponseDTO();
         CoordsDTO coordsDTO1 = new CoordsDTO();
         coordsDTO1.setLat(String.valueOf(userLatitude + 0.005).substring(0,7));
         coordsDTO1.setLng(String.valueOf(userLongitude + 0.002).substring(0,7));
-//        coordsDTO1.setLng(new BigDecimal(String.valueOf(userLongitude)).add(new BigDecimal("0.002")).toString());
-//        coordsDTO1.setLng("49.8475");
-//        coordsDTO1.setLat("40.3776");
         clientSearchResponseDTO1.setCoords(coordsDTO1);
         clientSearchResponseDTO1.setTitle("E-karyera mərkəzi");
         clientSearchResponseDTO1.setDescription("Sevimli book kafe");
@@ -59,12 +53,8 @@ public class PlaceServiceImpl implements PlaceService {
 
         ClientSearchResponseDTO clientSearchResponseDTO2 = new ClientSearchResponseDTO();
         CoordsDTO coordsDTO2 = new CoordsDTO();
-//        coordsDTO2.setLat(String.valueOf(userLatitude + 0.003).substring(0,7));
-//        coordsDTO2.setLng(String.valueOf(userLongitude - 0.002).substring(0,7));
-        coordsDTO2.setLat(new BigDecimal(String.valueOf(userLatitude)).add(new BigDecimal("0.003")).toString());
-        coordsDTO2.setLng(new BigDecimal(String.valueOf(userLongitude)).subtract(new BigDecimal("0.002")).toString());
-//        coordsDTO2.setLng("49.8490");
-//        coordsDTO2.setLat("40.3790");
+        coordsDTO2.setLat(String.valueOf(userLatitude + 0.003).substring(0,7));
+        coordsDTO2.setLng(String.valueOf(userLongitude - 0.002).substring(0,7));
         clientSearchResponseDTO2.setCoords(coordsDTO2);
         clientSearchResponseDTO2.setTitle("Sağlamlıq futbol meydançası");
         clientSearchResponseDTO2.setDescription("Hər gün 10:00-dan 16:00-dək açıqdır.");
@@ -80,18 +70,18 @@ public class PlaceServiceImpl implements PlaceService {
         clientSearchResponseDTO3.setDescription("Nisyə mal verilmir");
         clientSearchResponseDTO3.setDiscount(0);
         clientSearchResponseDTO3.setId(UUID.randomUUID().toString());
-/*
+
         ClientSearchResponseDTO clientSearchResponseDTO4 = new ClientSearchResponseDTO();
         CoordsDTO coordsDTO4 = new CoordsDTO();
-        coordsDTO4.setLat(String.valueOf(userLatitude - 0.003).substring(0,7));
-        coordsDTO4.setLng(String.valueOf(userLongitude + 0.003).substring(0,7));
+        coordsDTO4.setLat(String.valueOf(userLatitude));
+        coordsDTO4.setLng(String.valueOf(userLongitude));
         clientSearchResponseDTO4.setCoords(coordsDTO4);
         clientSearchResponseDTO4.setTitle("Məhəllə market");
         clientSearchResponseDTO4.setDescription("Nisyə mal verilmir");
         clientSearchResponseDTO4.setDiscount(0);
         clientSearchResponseDTO4.setId(UUID.randomUUID().toString());
-*/
-        return List.of(clientSearchResponseDTO1, clientSearchResponseDTO2/*, clientSearchResponseDTO3, clientSearchResponseDTO4*/);
+
+        return List.of(clientSearchResponseDTO1, clientSearchResponseDTO2, clientSearchResponseDTO3, clientSearchResponseDTO4);
     }
 
 }
