43a9428c32f391e9932eb2b6f4dc280dd98a8c6e
Javid Afandiyev <afandiyevcavid@gmail.com>
2022-01-23 10:37:42 +0400

    test commit 3

--- src/main/java/az/interestmap/interestmap/service/impl/PlaceServiceImpl.java
+++ src/main/java/az/interestmap/interestmap/service/impl/PlaceServiceImpl.java
@@ -12,6 +12,7 @@ import az.interestmap.interestmap.service.PlaceService;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
@@ -44,8 +45,10 @@ public class PlaceServiceImpl implements PlaceService {
         CoordsDTO coordsDTO1 = new CoordsDTO();
 //        coordsDTO1.setLat(String.valueOf(userLatitude + 0.005).substring(0,7));
 //        coordsDTO1.setLng(String.valueOf(userLongitude + 0.002).substring(0,7));
-        coordsDTO1.setLng("49.8475");
-        coordsDTO1.setLat("40.3776");
+        coordsDTO1.setLat(new BigDecimal(String.valueOf(userLatitude)).add(new BigDecimal("0.005")).toString());
+        coordsDTO1.setLat(new BigDecimal(String.valueOf(userLongitude)).add(new BigDecimal("0.002")).toString());
+//        coordsDTO1.setLng("49.8475");
+//        coordsDTO1.setLat("40.3776");
         clientSearchResponseDTO1.setCoords(coordsDTO1);
         clientSearchResponseDTO1.setTitle("E-karyera mərkəzi");
         clientSearchResponseDTO1.setDescription("Sevimli book kafe");
@@ -56,8 +59,10 @@ public class PlaceServiceImpl implements PlaceService {
         CoordsDTO coordsDTO2 = new CoordsDTO();
 //        coordsDTO2.setLat(String.valueOf(userLatitude + 0.003).substring(0,7));
 //        coordsDTO2.setLng(String.valueOf(userLongitude - 0.002).substring(0,7));
-        coordsDTO2.setLng("49.8490");
-        coordsDTO2.setLat("40.3790");
+        coordsDTO2.setLat(new BigDecimal(String.valueOf(userLatitude)).add(new BigDecimal("0.003")).toString());
+        coordsDTO2.setLat(new BigDecimal(String.valueOf(userLongitude)).subtract(new BigDecimal("0.002")).toString());
+//        coordsDTO2.setLng("49.8490");
+//        coordsDTO2.setLat("40.3790");
         clientSearchResponseDTO2.setCoords(coordsDTO2);
         clientSearchResponseDTO2.setTitle("Sağlamlıq futbol meydançası");
         clientSearchResponseDTO2.setDescription("Hər gün 10:00-dan 16:00-dək açıqdır.");
